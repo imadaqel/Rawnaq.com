6ac5a3aa39d4323626e69a04b1fdc167bca4bb2d
Rasha <r.alamleh@gmail.com>
2020-12-03 17:20:17 +0200

    Navbar

--- shoppingPro/shopping-react/src/components/App.js
+++ shoppingPro/shopping-react/src/components/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './navBar.js';
 
 function App() {
   return (
     <div className="App">
+    <NavBar />
       <header className="App-header">
         <h1>RAWNAQ.COM</h1>
         <img id ='img' src="https://image.freepik.com/free-vector/shopping-online-website-mobile-application-landing-page-concept-marketing-digital-marketing_144352-77.jpg"  alt="" />
